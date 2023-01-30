9675b708300c66426174680cd3c19eed6b2cffbf
Icarus-afk <ahmed.ehsan1258@gmail.com>
2023-01-30 10:26:25 +0600

    Temporary solution problem 8

--- problem_20/main.go
+++ problem_20/main.go
@@ -26,17 +26,68 @@ func (s *Stack) Peek() string {
 	return s.data[len(s.data)-1]
 }
 
+func (s *Stack) Len() int {
+	return len(s.data)
+}
+
+func (s *Stack) Top() string {
+	if s.IsEmpty() {
+		return ""
+	} else {
+		index := len(s.data) - 1
+		elem := (s.data)[index]
+		return elem
+	}
+}
+
+func isValid(s *Stack) bool {
+	stack := new(Stack)
+	x := len(stack.data)
+	for v := range x {
+		if v == 40 || v == 91 || v == 123 {
+			stack.Push(string(v))
+		}
+
+		if v == 41 {
+			if stack.Top() == "(" {
+				stack.Pop()
+			} else {
+				return false
+			}
+		}
+		if v == 93 {
+			if stack.Top() == "[" {
+				stack.Pop()
+			} else {
+				return false
+			}
+		}
+		if v == 125 {
+			if stack.Top() == "{" {
+				stack.Pop()
+			} else {
+				return false
+			}
+		}
+	}
+	if len := stack.Len(); len > 0 {
+		return false
+	}
+	return true
+}
+
 func main() {
 	var input string
 	stack := &Stack{}
 	fmt.Println("Enter values to add to the stack, separated by spaces:")
 	for true {
 		fmt.Scan(&input)
-		if input == "\t" {
+		if input == "~" { //temporary solution for breaking out of the loop
 			break
 		}
 		stack.Push(input)
 
 		fmt.Println("Stack:", stack.data)
 	}
+	fmt.Println(isValid(stack))
 }
